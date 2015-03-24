9d9f227d950923ff96110ab5287789178154be56
DonaldZhong <donaldz@sfu.ca>
2015-03-23 23:49:28 -0700

    Moved data into JSON file and added markers
    remove hard coded coordinates and moved them to data.json.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -8,18 +8,44 @@
  * Controller of the 381FinalApp
  */
 angular.module('381FinalApp')
-  .controller('MainCtrl', function ($scope) {
-		$scope.map = { center: { latitude: 49.2827, longitude: -123.1207 }, zoom: 13 };
-		//aussie pie guy
-		$scope.marker = { id: 0, coords: { latitude: 49.284782, longitude: -123.114800}};
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+		$scope.map = { 
+			center: { 
+				latitude: 49.2827, 
+				longitude: -123.1207 
+			}, 
 
-		$scope.windowOptions = { visible: false };
-		$scope.onClick = function() {
-			$scope.windowOptions.visible = !$scope.windowOptions.visible;
+			zoom: 13 };
+
+		$scope.markers = [];
+		var idCount = 0;
+
+		$http.get('data.json').success(function(data) {
+			$scope.trucks = data;
+
+			 angular.forEach(data, function(object) { 
+			 	var marker = {
+			 		id: idCount, 
+			 		coords: {
+			 			latitude: object.latitude, 
+			 			longitude: object.longitude
+			 		}};
+				$scope.markers.push(marker);
+				idCount++;
+				$scope.windowOptions = { visible: false };
+					$scope.onClick = function() {
+					$scope.windowOptions.visible = !$scope.windowOptions.visible;
 		};
-
 		$scope.closeClick = function() {
 			$scope.windowOptions.visible = false;
 		};
 
-  });
+
+			});
+		});
+
+
+
+
+
+  }]);
