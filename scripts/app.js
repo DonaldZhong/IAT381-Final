5afd8a69c70cec8519eba8371cb62135e57a3f78
DonaldZhong <donaldz@sfu.ca>
2015-04-02 02:01:46 -0700

    Moved main controller into app.js, minor UI changes
    toolbar now persistent across all pages

--- scripts/app.js
+++ scripts/app.js
@@ -51,3 +51,37 @@ var APP = angular
       .primaryPalette('blue')
       .accentPalette('red');
   });
+
+
+APP.controller('MainCtrl', function ($scope, $location, $timeout, $mdBottomSheet) {
+    
+
+    $scope.openBottomSheet = function($event) {
+      $scope.alert = '';
+      $mdBottomSheet.show({
+        templateUrl: 'views/sheetTemplate.html',
+        controller: 'GridBottomSheetCtrl',
+        targetEvent: $event
+      }).then(function(clickedItem) {
+        $scope.alert = clickedItem.name + ' clicked!';
+          $location.path (clickedItem.urlPath);
+
+    });
+  };
+});
+
+APP.controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
+    $scope.items = [
+      {name: 'Home',    icon: 'images/home.png', urlPath:'/'},
+      {name: 'Map',     icon: 'test',      urlPath: '/map'},
+      {name: 'Trucks',  icon: 'test',      urlPath:'/trucks'},
+      {name: 'About',   icon: 'test',      urlPath:'/about'},
+    ];
+
+    $scope.listItemClick = function($index) {
+      var clickedItem = $scope.items[$index];
+      console.log(clickedItem);
+      $mdBottomSheet.hide(clickedItem);
+    };
+ });
+
